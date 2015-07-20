62c9c392662d2d1b0255efeb60e93b9017a4defd
adam.kozuch@gmail.com <adam.kozuch@gmail.com>
2015-07-20 16:53:54 +0200

    aaa

--- file.scala
+++ file.scala
@@ -5,47 +5,62 @@ build a collection type even if it is nested.
 
 
 I started with:
-TableExpansion
-	TableNode
-	TypeMapping
-		ProductNode
-			TableExpansion	
-				TableNode
-				TypeMapping
-					Select
-					Select
-			Select 
-			select    // here should be an array
-			TableExpansion   //this should be array of objects
-				TableNode
-				Typemapping 
-					Select
+Bind
+	TableExpansion
+		TableNode
+		TypeMapping
+			ProductNode
+				TableExpansion	
+					TableNode
+					TypeMapping
+						Select
+						Select
+				Select 
+				select    // here should be an array
+				TableExpansion   //this should be array of objects
+					TableNode
+					Typemapping 
+						Select
+	Pure
+		TableExpansion	
+			TableNode
+			TypeMapping
+				Select
+				Select
+			
 
 		
 				
 
 I ended with sth like:
-
-StructNode	
-	(AnonSymbol, StructNode)
-		AnonSymbol
-		StructNode
-			(ElementSymbol,Select)
-			(elementSymbol,Select)
-	(ElementSymbol, Select)
-		ElementSymbol
-		Select
-	(AnonSymbol, Pure(Select))
-		AnonSymbol
-		Pure
-			Select(Path, SomeSymbol)
-	(AnonSymbol, Pure(StructNode))
-			AnonSymbol
+ResultConverter
+	StructNode (I need a name for this structNode)	
+		(FieldSymbol, StructNode)
+			FieldSymbol (not sure if field symbol is the right choice)
+			StructNode
+				(ElementSymbol,Select)
+				(elementSymbol,Select)
+		(ElementSymbol, Select)
+			ElementSymbol
+			Select
+		(ElementSymbol, Pure(Select))   (not sure if ElementSymbol is right choice for  primitive collection)
+			ElementSymbol
 			Pure
-				StructNode
-					(ElementSymbol, Select)
+				Select
+		(FieldSymbol, Pure(StructNode))
+				FieldSymbol
+				Pure
+					StructNode
+						(ElementSymbol, Select)
+	Pure
+		FieldSymbol
+			StructNode
+				(ElementSymbol,Select)
+				(elementSymbol,Select)
+		
+	
 
-
+That will be part of my operations.
 
 
 
